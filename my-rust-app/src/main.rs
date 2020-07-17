51967ce1cff94dbb36f9419ce1a465791d764e0f
ynishimura <ynishimura0922@gmail.com>
2020-07-17 17:22:05 +0900

    引数を取る

--- my-rust-app/src/main.rs
+++ my-rust-app/src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "my-rust-app")]
+struct ProgramOption {
+    /// 表示対象のファイルを指定する
+    #[structopt(parse(from_os_str))]
+    file: PathBuf,
+
+    /// テーマ名を指定する
+    #[structopt(short, long = "theme")]
+    theme_name: Option<String>,
+}
+
+fn parse_args() -> ProgramOption {
+    ProgramOption::from_args()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let options = parse_args();
+    // println!("options:{:?}", options);
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
